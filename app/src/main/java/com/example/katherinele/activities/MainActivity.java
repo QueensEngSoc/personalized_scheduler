e215ad3f776440ec8bd2fd9aaf48d9c339f1a8b3
fazeelabbasi <44741511+fazeelabbasi@users.noreply.github.com>
2019-03-30 20:27:07 -0400

    Fixed error

--- app/src/main/java/com/example/katherinele/activities/MainActivity.java
+++ app/src/main/java/com/example/katherinele/activities/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        
 
         Button button = (Button) findViewById(R.id.email_sign_in_button);
 
